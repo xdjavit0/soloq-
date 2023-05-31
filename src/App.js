5348f7a4306486dc03ddbef43b356ee6b4f2851d
Javier-Carrascosaromero_tvlport <javier.Carrascosaromero@travelport.com>
2023-05-31 22:28:21 +0200

    feat: :fire:initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Login } from './Login';
+import { Register } from './Register';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
